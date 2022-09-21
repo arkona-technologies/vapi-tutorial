d83f37da0c0d8007fbb04c8e944b81ed1f9fe767
Jonas Reucher <J.Reucher@arkona-techonologies.de>
2022-09-21 14:47:47 +0200

    [make] export environment var to npm directly

--- make.sh
+++ make.sh
@@ -4,12 +4,12 @@ while [ 1 -gt 0 ]
 do
     echo -e "Please enter the IP-Address of your AT300/C100:\c"
     read BLADE
-               echo "Check if Blade is reachable?"
                if [[ $(echo $BLADE | grep -E '^(http|https)' | wc -l) -gt 0 ]]
                then 
                   echo "please omit prefixes such http(s) etc..."
                   continue;
                fi
+               echo "Check if Blade is reachable..."
                if [ $(curl -LI http://$BLADE -o /dev/null -w '%{http_code}\n' -s) == "200" ]; 
                then 
                   echo "Blade seems to exist. Starting setup"
@@ -20,16 +20,16 @@ do
                   echo $BLADE_URL
                   export BLADE_URL
                   echo "Installing dependencies..."
-                  npm run setup 
+                  BLADE_URL=$BLADE_URL npm run setup 
                   echo "Compiling Kiosk..."
                   tsc -p tsconfig_kiosk.json
                   echo "Compiling Scripts..."
                   tsc -p tsconfig.json
                   cp ./blade.json ./.build/
-                  echo "You can start the interactive webpage via ./runkiosk.sh or use the scripts under .build"
+                  echo "You can start the interactive webpage via ./runkiosk.sh or use the scripts under .build directly"
                   break;
             else
-            echo "Blade doesn't seem to exist"
+            echo "Blade is unreachable ¯\_(ツ)_/¯"
             fi
 done
 
